424baaa31fa0aace8c3c2482cbf66dc6bb61dd80
josh <josh2013.880922@gmail.com>
2021-05-11 18:48:03 +0800

    code review, bug fix;

--- app/src/main/java/com/josh/smartkettlebell/ui/main/challenge/ChallengeFragment.java
+++ app/src/main/java/com/josh/smartkettlebell/ui/main/challenge/ChallengeFragment.java
@@ -23,7 +23,7 @@ import static android.content.Context.MODE_PRIVATE;
 
 
 public class ChallengeFragment extends Fragment {
-    TextView textView, timeText, challengeText, challengeText2, challengeTimes, challengeTimes2, Set;
+    TextView  timeText, challengeText, challengeText2, challengeTimes, challengeTimes2, Set;
     GifImageView gif1, gif2;
     Button gotoChallengeBtn;
     public static String PREFERENCE_NAME_CHALLENGE = "CHALLENGE";
@@ -43,11 +43,6 @@ public class ChallengeFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_challenge, container, false);
 
-//        ActionBar actionBar = getSupportActionBar();
-//        actionBar.setDisplayHomeAsUpEnabled(true);
-
-
-        textView = view.findViewById(R.id.txt);
         challengeText = view.findViewById(R.id.challengetext);
         challengeText2 = view.findViewById(R.id.challengetext2);
         challengeTimes = view.findViewById(R.id.challengetimes);
@@ -61,183 +56,117 @@ public class ChallengeFragment extends Fragment {
         //讀取當天日期
         timeText = view.findViewById(R.id.txt2);
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy年MM月dd日", Locale.getDefault());
-        Date curDate = new Date(System.currentTimeMillis()); // 獲取當前時間
+        Date curDate = new Date(); // 獲取當前時間
         String today = formatter.format(curDate);
 
         //建立SharedPreferences
         challengePref = getActivity().getSharedPreferences(PREFERENCE_NAME_CHALLENGE, MODE_PRIVATE);
-
         String date = challengePref.getString(PREFERENCE_KEY_DATE, "");//讀取儲存的日期
-
-
         timeText.setText(today);
-
-
-        if (!date.equals(today)) { //是否已換日
+        //是否已換日
+        if (!date.equals(today)) {
             //更新存入的日期
             challengePref.edit()
                     .putString(PREFERENCE_KEY_DATE, today)
                     .apply();
 
-
-            String dailyChallenge = changeChallenge();//呼叫changeChallenge
-            String[] dailyChallenges = dailyChallenge.split(",");
             //儲存今日的挑戰
             challengePref.edit()
-                    .putString(PREFERENCE_KEY_CHALLENGE, dailyChallenge)
+                    .putString(PREFERENCE_KEY_CHALLENGE, changeChallenge())
                     .apply();
-            //顯示今日的挑戰
-            challengeText.setText(dailyChallenges[0]);
-            challengeText2.setText(dailyChallenges[2]);
-            challengeTimes.setText(dailyChallenges[1]);
-            challengeTimes2.setText(dailyChallenges[3]);
-            Set.setText(dailyChallenges[4]);
-
-            switch (dailyChallenges[0]) {
-                case "壺鈴擺盪":
-                    gif1.setImageResource(R.drawable.kettlebell_swings);
-                    break;
-                case "壺鈴硬舉":
-                    gif1.setImageResource(R.drawable.kettlebell_deadlift);
-                    break;
-                case "壺鈴推舉":
-                    gif1.setImageResource(R.drawable.kettlebell_press);
-                    break;
-                case "壺鈴單手划船":
-                    gif1.setImageResource(R.drawable.kettlebell_bent_over_row);
-                    break;
-                case "壺鈴深蹲":
-                    gif1.setImageResource(R.drawable.kettlebell_squat);
-                    break;
-            }
-
-            switch (dailyChallenges[2]) {
-                case "壺鈴擺盪":
-                    gif2.setImageResource(R.drawable.kettlebell_swings);
-                    break;
-                case "壺鈴硬舉":
-                    gif2.setImageResource(R.drawable.kettlebell_deadlift);
-                    break;
-                case "壺鈴推舉":
-                    gif2.setImageResource(R.drawable.kettlebell_press);
-                    break;
-                case "壺鈴單手划船":
-                    gif2.setImageResource(R.drawable.kettlebell_bent_over_row);
-                    break;
-                case "壺鈴深蹲":
-                    gif2.setImageResource(R.drawable.kettlebell_squat);
-                    break;
-            }
-
-
-        } else {
-            String dailyChallenge = challengePref.getString(PREFERENCE_KEY_CHALLENGE, "");
-            String[] dailyChallenges = dailyChallenge.split(",");
-            //顯示今日的挑戰
-            challengeText.setText(dailyChallenges[0]);
-            challengeText2.setText(dailyChallenges[2]);
-            challengeTimes.setText(dailyChallenges[1]);
-            challengeTimes2.setText(dailyChallenges[3]);
-            Set.setText(dailyChallenges[4]);
-
-            //根據今天的挑戰抓GIF
-            switch (dailyChallenges[0]) {
-                case "壺鈴擺盪":
-                    gif1.setImageResource(R.drawable.kettlebell_swings);
-                    break;
-                case "壺鈴硬舉":
-                    gif1.setImageResource(R.drawable.kettlebell_deadlift);
-                    break;
-                case "壺鈴推舉":
-                    gif1.setImageResource(R.drawable.kettlebell_press);
-                    break;
-                case "壺鈴單手划船":
-                    gif1.setImageResource(R.drawable.kettlebell_bent_over_row);
-                    break;
-                case "壺鈴深蹲":
-                    gif1.setImageResource(R.drawable.kettlebell_squat);
-                    break;
-            }
-
-            switch (dailyChallenges[2]) {
-                case "壺鈴擺盪":
-                    gif2.setImageResource(R.drawable.kettlebell_swings);
-                    break;
-                case "壺鈴硬舉":
-                    gif2.setImageResource(R.drawable.kettlebell_deadlift);
-                    break;
-                case "壺鈴推舉":
-                    gif2.setImageResource(R.drawable.kettlebell_press);
-                    break;
-                case "壺鈴單手划船":
-                    gif2.setImageResource(R.drawable.kettlebell_bent_over_row);
-                    break;
-                case "壺鈴深蹲":
-                    gif2.setImageResource(R.drawable.kettlebell_squat);
-                    break;
-            }
         }
 
+        String[] dailyChallenges = challengePref.getString(PREFERENCE_KEY_CHALLENGE, "")
+                .split(",");
+        //顯示今日的挑戰
+        challengeText.setText(dailyChallenges[0]);
+        challengeText2.setText(dailyChallenges[2]);
+        challengeTimes.setText(dailyChallenges[1]);
+        challengeTimes2.setText(dailyChallenges[3]);
+        Set.setText(dailyChallenges[4]);
+
+        //根據今天的挑戰抓GIF
+        setGif(gif1,dailyChallenges[0]);
+        setGif(gif2,dailyChallenges[2]);
 
         return view;
     }
 
+    void setGif(GifImageView gif,String dailyChallenge){
+        switch (dailyChallenge) {
+            case "壺鈴擺盪":
+                gif.setImageResource(R.drawable.kettlebell_swings);
+                break;
+            case "壺鈴硬舉":
+                gif.setImageResource(R.drawable.kettlebell_deadlift);
+                break;
+            case "壺鈴推舉":
+                gif.setImageResource(R.drawable.kettlebell_press);
+                break;
+            case "壺鈴單手划船":
+                gif.setImageResource(R.drawable.kettlebell_bent_over_row);
+                break;
+            case "壺鈴深蹲":
+                gif.setImageResource(R.drawable.kettlebell_squat);
+                break;
+        }
+    }
 
     public String changeChallenge() {
-        StringBuilder challenge = new StringBuilder();
-        StringBuilder times = new StringBuilder();
-        String ugoki = "";
-        int oldnumber = 0;
+        StringBuilder challengeString = new StringBuilder();
+        String times = "";
+        String exerciseName = "";
+        int oldNumber = 0;
         Random random = new Random();
         for (int i = 0; i < 2; i++) {
             int number = random.nextInt(5) + 1;
             //第二個動作不與第一個動作重複
-            while (oldnumber == number) {
+            while (oldNumber == number) {
                 number = random.nextInt(5) + 1;
             }
             //儲存上個動作的編號
-            oldnumber = number;
+            oldNumber = number;
 
             switch (number) {
                 case 1:
-                    ugoki = "壺鈴擺盪";
+                    exerciseName = "壺鈴擺盪";
                     break;
                 case 2:
-                    ugoki = "壺鈴硬舉";
+                    exerciseName = "壺鈴硬舉";
                     break;
                 case 3:
-                    ugoki = "壺鈴推舉";
+                    exerciseName = "壺鈴推舉";
                     break;
                 case 4:
-                    ugoki = "壺鈴單手划船";
+                    exerciseName = "壺鈴單手划船";
                     break;
                 case 5:
-                    ugoki = "壺鈴深蹲";
+                    exerciseName = "壺鈴深蹲";
                     break;
             }
-            ugoki = ugoki + ",";
+            exerciseName += ",";
 
             int rand = random.nextInt(4) + 1;
             switch (rand) {
                 case 1:
-                    times = new StringBuilder("5下");
+                    times = "5下";
                     break;
                 case 2:
-                    times = new StringBuilder("10下");
+                    times = "10下";
                     break;
                 case 3:
-                    times = new StringBuilder("15下");
+                    times = "15下";
                     break;
                 case 4:
-                    times = new StringBuilder("20下");
+                    times = "20下";
                     break;
             }
-            times.append(",");
-            challenge.append(ugoki).append(times);
+            times += ",";
+            challengeString.append(exerciseName).append(times);
         }
 
         int setTimes = random.nextInt(3) + 1;
-        challenge.append("共").append(setTimes).append("組");
-        return challenge.toString();
+        challengeString.append("共").append(setTimes).append("組");
+        return challengeString.toString();
     }
 }
